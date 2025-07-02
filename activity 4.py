ebc98021419791777e2c053c32633a19670ee894
Delisa0 <elahidelisa0@gmail.com>
2025-07-02 20:12:41 +1000

    lesson 3

---
+++ activity 4.py
@@ -0,0 +1,7 @@
+import datetime
+current_time=datetime.datetime.now()
+print("Time now at Greenwhich Meridian Time: ",end="")
+print(current_time)
+
+import calendar
+print("\n", calendar.calendar(2009))
